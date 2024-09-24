66cd0be5dd47955297c7c0f0c090a6cc53479b78
dineshchahar <dineshchahar4666@gmail.com>
2024-09-24 09:56:11 +0530

    new laptop soon

--- googlelocation/models.py
+++ googlelocation/models.py
@@ -1,17 +1,98 @@
 from django.db import models
 
 class SourceLocation(models.Model):
-    Source_lat = models.FloatField(max_length=100)
-    Source_lon = models.FloatField(max_length=100)
+    Source_lat = models.FloatField()
+    Source_lon = models.FloatField()
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['Source_lat', 'Source_lon'], name='unique_source_location')
+        ]
+    def __str__(self):
+        return f"lat:{self.Source_lat},long:{self.Source_lon}"
     
-
-
-# Create your models here.
-class Place(models.Model):
+class Competitor(models.Model):
+    # Burger king / KFC/pizza Hut/MacD/domino'z
     name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
     address = models.CharField(max_length=300)
     coordinates = models.TextField(max_length=300)
     types = models.TextField(max_length=500)
-    rating = models.FloatField(max_length=10)
-    user_rating = models.FloatField(max_length=10)
-    Distance = models.FloatField(max_length=10)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+    
+class SuperMarket(models.Model):
+    # Mall / supermarket / vishal mega mart/ reliance digital/croma/D-mart/Trends/smart bazaar
+    name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
+    address = models.CharField(max_length=300)
+    coordinates = models.TextField(max_length=300)
+    types = models.TextField(max_length=500)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+
+class PublicServices(models.Model):
+    # School/ College/BUS stand/ Metro Station / university/Cinema/movie/multiplex
+    name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
+    address = models.CharField(max_length=300)
+    coordinates = models.TextField(max_length=300)
+    types = models.TextField(max_length=500)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+
+class Apartments(models.Model):
+    name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
+    address = models.CharField(max_length=300)
+    coordinates = models.TextField(max_length=300)
+    types = models.TextField(max_length=500)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+class Retails(models.Model):
+    name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
+    address = models.CharField(max_length=300)
+    coordinates = models.TextField(max_length=300)
+    types = models.TextField(max_length=500)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+
+class Restaurants(models.Model):
+    name = models.CharField(max_length=255)
+    status= models.CharField(max_length=25,default='default')
+    address = models.CharField(max_length=300)
+    coordinates = models.TextField(max_length=300)
+    types = models.TextField(max_length=500)
+    rating = models.CharField(max_length=10)
+    user_rating = models.CharField(max_length=10)
+    Travel_Distance = models.CharField(max_length=10)
+    Travel_Time = models.CharField(max_length=10)
+    source_location = models.ForeignKey(SourceLocation,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return f"Name:{self.name},Address:{self.address},Coordinates:{self.coordinates},Types:{self.types},Rating:{self.rating},User_Rating:{self.user_rating},Travel_Distance:{self.Travel_Distance},Travel_Time:{self.Travel_Time}"
+
--- googlelocation/views.py
+++ googlelocation/views.py
@@ -1,64 +1,265 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse 
 from .forms import LocationForm
-from .models import Place
+from .models import SourceLocation, SuperMarket,PublicServices,Competitor,Apartments,Retails,Restaurants
 import json
 from django import views
 import googlemaps
 
-
-class LocationView(views):
-    template_name = 'googlelocation/home.html'
-    google_api_key = 'AIzaSyCNJ0iv-Why9tmFmkTZrIVPp_B_qMZYQpg'
-    def get(self,request,*args,**kwargs):
-        form = LocationForm()
-        return render(request,self.template_name,{'form':form})
-    def post(self,request,*args,**kwargs):
+def home(request):
+    if request.method == 'POST':
         form = LocationForm(request.POST)
-        context = {}
         if form.is_valid():
-            location  = form.cleaned_data['location']
+            location = form.cleaned_data['location']
             radius = form.cleaned_data['radius']
-            try:
-                latitude, longitude = map(float,location.strip().split(','))
-            except:
-                pass
-
-
-# def home(request):
-#     if request.method == 'POST':
-#         form = LocationForm(request.POST)
-#         if form.is_valid():
-#             location = form.cleaned_data['location']
-#             radius = form.cleaned_data['radius']
+            latitude,longitude = map(float,location.split(','))
+            Google_API_KEY = 'AIzaSyCNJ0iv-Why9tmFmkTZrIVPp_B_qMZYQpg'
+            SourceLocation.objects.all().delete()
+            SuperMarket.objects.all().delete()
+            PublicServices.objects.all().delete()
+            Competitor.objects.all().delete()
+            Apartments.objects.all().delete()
+            Retails.objects.all().delete()
+            Restaurants.objects.all().delete()
             
-#             try:
+            gmaps = googlemaps.Client(key=Google_API_KEY)
 
-#                 longitude,latitude = map(float,location.split(','))
-#                 Google_API_KEY = 'AIzaSyCNJ0iv-Why9tmFmkTZrIVPp_B_qMZYQpg'
+            # source database
+            source_location = SourceLocation.objects.create(
+                Source_lat = latitude,
+                Source_lon = longitude)
+            # competitors database
+            competitor = ['KFC','Burger King',"McDonald's",'Pizza Hut',"Domino's"]
+            nearby_competitors = []
+            for comp in competitor:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = comp)
+                if places_result['status'] == 'OK':
+                    nearby_competitors.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_competitors:
+                origin = (latitude,longitude)
+                coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                if result['status']=='OK':
+                    element = result['rows'][0]['elements'][0]
+                    if element['status'] == 'OK':
+                        distance = element['distance']['text']
+                        duration = element['duration']['text']
+                    else:
+                        distance = "cann't find"
+                        duration = "cann't find"
+                Competitor.objects.create(
+                    name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                )
+            
 
-#                 context = {
-#                     'longitude':longitude,
-#                     'latitude' : latitude,
-#                     'radius':radius,
-#                     'form' : form,
-#                     'success':True,
-#                 }
-#             except:
-#                 context={
-#                     'form':form,
-#                     'error': 'Invalid location format. Please use "longitide, latitude format."'
-#                 }
-#         else:
-#             context = {
-#                 'form': form,
-#                 'error': 'Invalid input. Please correct the errors below.'
-#             }
-        
-#     else:
-#         form = LocationForm()
-#         context = {'form':form}
-#     return render(request, 'googlelocation/home.html',context)
+            # supermarket database
+            nearby_supermarket = []    
+            supermarket = ['Mall','Supermarket','Vishal Mega mart','Croma','D-mart','Trends','smart bazaar','Reliance Digital']
+            for market in supermarket:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = market)
+                if places_result['status'] == 'OK':
+                    nearby_supermarket.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_supermarket:
+                origin = (latitude,longitude)
+                coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                if result['status']=='OK':
+                    element = result['rows'][0]['elements'][0]
+                    if element['status'] == 'OK':
+                        distance = element['distance']['text']
+                        duration = element['duration']['text']
+                    else:
+                        distance = "cann't find"
+                        duration = "cann't find"
+                SuperMarket.objects.create(
+                    name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                )
+            # PublicServices database 
+            nearby_publicservices = []  
+            publicservices = ['Bus Stand','Metro Station','School','college','University','Tourist']  
+            
+            for public in publicservices:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = public)
+                if places_result['status'] == 'OK':
+                    nearby_publicservices.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_publicservices:
+                origin = (latitude,longitude)
+                coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                if result['status']=='OK':
+                    element = result['rows'][0]['elements'][0]
+                    if element['status'] == 'OK':
+                        distance = element['distance']['text']
+                        duration = element['duration']['text']
+                    else:
+                        distance = "cann't find"
+                        duration = "cann't find"
+                PublicServices.objects.create(
+                    name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                )
+            #Apartments database
 
+            nearby_apartments = [] 
+            apartments = ['Appartments'] 
+              
+            
+            for apartment in apartments:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = apartment)
+                if places_result['status'] == 'OK':
+                    nearby_apartments.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_apartments:
+                origin = (latitude,longitude)
+                coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                if result['status']=='OK':
+                    element = result['rows'][0]['elements'][0]
+                    if element['status'] == 'OK':
+                        distance = element['distance']['text']
+                        duration = element['duration']['text']
+                    else:
+                        distance = "cann't find"
+                        duration = "cann't find"
+                Apartments.objects.create(
+                    name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                )
+            # retails database
+            nearby_retails = [] 
+            retails = ['retail shop'] 
+              
+            
+            for retail in retails:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = retail)
+                if places_result['status'] == 'OK':
+                    nearby_retails.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_retails:
+                origin = (latitude,longitude)
+                coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                if result['status']=='OK':
+                    element = result['rows'][0]['elements'][0]
+                    if element['status'] == 'OK':
+                        distance = element['distance']['text']
+                        duration = element['duration']['text']
+                    else:
+                        distance = "cann't find"
+                        duration = "cann't find"
+                Retails.objects.create(
+                    name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                )
+            # restaurants Database
 
+            exclude_competitor = {'KFC','Burger King',"McDonald's",'Pizza Hut',"Domino's"}
+            nearby_restaurant = []
+            restaurants = ['Restaurant']
+            for restaurant in restaurants:
+                places_result = gmaps.places_nearby(location = (latitude,longitude),radius = radius,type = restaurant)
+                if places_result['status'] == 'OK':
+                    nearby_restaurant.extend(places_result.get('results', []))
+                else:
+                    continue
+            for place in nearby_restaurant:
+                if place['name'] not in exclude_competitor and (place.get('user_ratings_total',0) > 100) and (place.get('rating',0)): 
+                    origin = (latitude,longitude)
+                    coordinate = (place['geometry']['location']['lat'],place['geometry']['location']['lng'])
+                    result = gmaps.distance_matrix(origins=[origin], destinations=[coordinate], mode='driving', departure_time='now')
+                    if result['status']=='OK':
+                        element = result['rows'][0]['elements'][0]
+                        if element['status'] == 'OK':
+                            distance = element['distance']['text']
+                            duration = element['duration']['text']
+                        else:
+                            distance = "cann't find"
+                            duration = "cann't find"
+                    Restaurants.objects.create(
+                        name = place.get('name','No Name'),
+                    status = place.get('business_status','default'),
+                    address = place.get('vicinity','unknown'),
+                    coordinates = coordinate,
+                    types = place.get('types',[]),
+                    rating = place.get('rating',0),
+                    user_rating = place.get('user_ratings_total',0),
+                    Travel_Distance = distance,
+                    Travel_Time = duration,
+                    source_location = source_location
+                    )
+            return redirect('result')    
+    else:
+        form = LocationForm()
+
+    return render(request, 'googlelocation/home.html',{'form':form})
+def result(request):
+    sourcelocation = SourceLocation.objects.all()
+    competitors = Competitor.objects.all()
+    supermarkets = SuperMarket.objects.all()
+    publicservices = PublicServices.objects.all()
+    apartments = Apartments.objects.all()
+    retails = Retails.objects.all()
+    restaurants = Restaurants.objects.all()
+    context = {
+        'sourcelocation':sourcelocation,
+        'competitors':competitors,
+        'publicservices':publicservices,
+        'retails':retails,
+        'restaurants': restaurants,
+        'apartments': apartments,
+        'supermarkets': supermarkets,
+    }
+    print(context)
+    
+    return render(request, 'googlelocation/result.html', context)
 
