363ec3a18221729b6d2d195a3c292e9f412461b1
Dinesh <chahardinesh07@gmail.com>
2024-10-09 10:55:31 +0530

    production

--- franchise_location/settings.py
+++ franchise_location/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cw&llexe!b4pz89wpfq+qses$2&8*$ev(3_i7b5+lxcny(!)!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.207.66.131']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
